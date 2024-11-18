2e132d8c81c934a926a4adeacd18af985d82254f
Densodim <25denso@gmail.com>
2024-11-18 22:47:12 +0300

    change file

--- api/server.js
+++ api/server.js
@@ -2,48 +2,23 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
 
-// Пример данных устройств
+// Статические файлы
+app.use(express.static(path.join(__dirname, '../public')));
+
 let devices = [
-    {
-        id: 1, name: 'pH-метр Mettler-Toledo', status: 'Свободен', notification: true, image: '/img/pH.png',
-        analytic: [
-            {
-                type: 'В работе',
-                date: new Date(2024, 10, 8, 15, 30),
-                sample: ['Образец/серия: 000100057935_170000010325_0000251849'],
-                user: 'morozovava',
-                result: true
-            }
-        ]
-    },
+    { id: 1, name: 'pH-метр Mettler-Toledo', status: 'Свободен', notification: true, image: '/img/pH.png' },
     { id: 2, name: 'Спектрофотометр Varian', status: 'Свободен', notification: true, image: '/img/cary50Bio.png' },
     { id: 3, name: 'Титратор', status: 'Свободен', notification: false, image: '/img/titrator.png' },
     { id: 4, name: 'Коагулометр Tcoag, KC 4 Delta', status: 'Свободен', notification: false, image: '/img/tcoag.png' }
 ];
 
-// Настройка статических файлов
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.json());
-
-// API маршруты
+// API для получения устройств
 app.get('/api/devices', (req, res) => {
     res.json(devices);
 });
 
-app.post('/api/devices/:id/toggle', (req, res) => {
-    const deviceId = parseInt(req.params.id);
-    const device = devices.find(d => d.id === deviceId);
-    if (device) {
-        device.notification = !device.notification;
-        res.json({ success: true, notification: device.notification });
-    } else {
-        res.status(404).json({ success: false, message: 'Device not found' });
-    }
-});
-
-// Маршрут для главной страницы
+// Главная страница
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
